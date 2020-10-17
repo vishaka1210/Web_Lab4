d064b15c32a92b381207cf7d679fa3222fb09f66
vishaka1210 <70833479+vishaka1210@users.noreply.github.com>
2020-10-17 10:03:02 +0530

    Update script4.js

--- script4.js
+++ script4.js
@@ -3,7 +3,8 @@ function findLMV(str)
 for(i=0;i<str.length;i++)
 {
 if(str.charAt(i)=='a' || str.charAt(i)=='e' || str.charAt(i)=='i' ||str.charAt(i)=='o' ||
-str.charAt(i)=='u' )
+str.charAt(i)=='u' || str.charAt(i)=='A' || str.charAt(i)=='E' || str.charAt(i)=='I' ||str.charAt(i)=='O' ||
+str.charAt(i)=='U' )
 return ("Left most vowel of " + str + " is at location " + (i+1) );
 }
 return ("No vowels found for string "+ str);
